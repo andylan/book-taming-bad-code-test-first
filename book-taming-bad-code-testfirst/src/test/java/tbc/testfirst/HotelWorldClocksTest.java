ad13b13f9aabdafc98b7f1cc63a3d8d854410235
Ben Wu <wubin28@gmail.com>
2014-04-25 22:20:51 +0800

    Added test the_time_of_clock_London_and_Moscow_should_be_1_and_5_respectively_after_the_phone_clock_is_set_to_9_Beijing_time() and wrote two assertions.

--- book-taming-bad-code-testfirst/src/test/java/tbc/testfirst/HotelWorldClocksTest.java
+++ book-taming-bad-code-testfirst/src/test/java/tbc/testfirst/HotelWorldClocksTest.java
@@ -20,4 +20,10 @@ public class HotelWorldClocksTest {
 
         assertEquals(1, londonClock.getLocalTime());
     }
+
+    @Test
+    public void the_time_of_clock_London_and_Moscow_should_be_1_and_5_respectively_after_the_phone_clock_is_set_to_9_Beijing_time() {
+        assertEquals(1, londonClock.getLocalTime());
+        assertEquals(5, moscowClock.getLocalTime());
+    }
 }
