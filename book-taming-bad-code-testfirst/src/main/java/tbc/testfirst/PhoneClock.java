cdf2291142056c47a91a9d1d5fd6211ef1f51327
Ben Wu <wubin28@gmail.com>
2014-04-25 19:46:39 +0800

    Created constructor of class PhoneClock with a parameter utcOffset.

--- book-taming-bad-code-testfirst/src/main/java/tbc/testfirst/PhoneClock.java
+++ book-taming-bad-code-testfirst/src/main/java/tbc/testfirst/PhoneClock.java
@@ -4,4 +4,10 @@ package tbc.testfirst;
  * Created by benwu on 14-4-25.
  */
 public class PhoneClock {
+
+    private final int utcOffset;
+
+    public PhoneClock(int utcOffset) {
+        this.utcOffset = utcOffset;
+    }
 }
