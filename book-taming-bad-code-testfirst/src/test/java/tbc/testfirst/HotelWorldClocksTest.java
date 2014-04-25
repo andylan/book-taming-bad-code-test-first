5cfa2be30c09cfbe349cdc2c64114381b6c89074
Ben Wu <wubin28@gmail.com>
2014-04-25 19:00:04 +0800

    Wrote an assertion for test the_time_of_London_clock_should_be_1_after_the_phone_clock_is_set_to_9_Beijing_time().

---
+++ book-taming-bad-code-testfirst/src/test/java/tbc/testfirst/HotelWorldClocksTest.java
@@ -0,0 +1,13 @@
+package tbc.testfirst;
+
+import org.junit.Test;
+
+/**
+ * Created by benwu on 14-4-25.
+ */
+public class HotelWorldClocksTest {
+    @Test
+    public void the_time_of_London_clock_should_be_1_after_the_phone_clock_is_set_to_9_Beijing_time() {
+        assertEquals(1, londonClock.getLocalTime());
+    }
+}
