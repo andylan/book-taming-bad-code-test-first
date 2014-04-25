69a63b120bd244ec27e3b986c9bdf50998ee3372
Ben Wu <wubin28@gmail.com>
2014-04-25 19:23:00 +0800

    Created constructor of CityClock with two parameters: cityName and utcOffset.

--- book-taming-bad-code-testfirst/src/main/java/tbc/testfirst/CityClock.java
+++ book-taming-bad-code-testfirst/src/main/java/tbc/testfirst/CityClock.java
@@ -4,4 +4,11 @@ package tbc.testfirst;
  * Created by benwu on 14-4-25.
  */
 public class CityClock {
+    private final String cityName;
+    private final int utcOffset;
+
+    public CityClock(String cityName, int utcOffset) {
+        this.cityName = cityName;
+        this.utcOffset = utcOffset;
+    }
 }
