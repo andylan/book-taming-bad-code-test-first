971855d327fb6c0a24e81640263b4cf088df5e71
Ben Wu <wubin28@gmail.com>
2014-04-25 19:07:41 +0800

    Created local variable londonClock.

--- book-taming-bad-code-testfirst/src/test/java/tbc/testfirst/HotelWorldClocksTest.java
+++ book-taming-bad-code-testfirst/src/test/java/tbc/testfirst/HotelWorldClocksTest.java
@@ -2,6 +2,8 @@ package tbc.testfirst;
 
 import org.junit.Test;
 
+import java.time.zone.ZoneOffsetTransitionRule;
+
 import static org.junit.Assert.assertEquals;
 
 /**
@@ -10,6 +12,7 @@ import static org.junit.Assert.assertEquals;
 public class HotelWorldClocksTest {
     @Test
     public void the_time_of_London_clock_should_be_1_after_the_phone_clock_is_set_to_9_Beijing_time() {
+        CityClock londonClock = new CityClock("London", 0);
         assertEquals(1, londonClock.getLocalTime());
     }
 }
