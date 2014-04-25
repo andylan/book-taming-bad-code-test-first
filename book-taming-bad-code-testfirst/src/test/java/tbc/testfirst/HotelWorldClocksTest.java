350397a09f4a1f00a3308663aa3f86bca38f3ccf
Ben Wu <wubin28@gmail.com>
2014-04-25 19:41:29 +0800

    Initialized a PhoneClock instance and invoked PhoneClock.setLocalTime() method in the test.

--- book-taming-bad-code-testfirst/src/test/java/tbc/testfirst/HotelWorldClocksTest.java
+++ book-taming-bad-code-testfirst/src/test/java/tbc/testfirst/HotelWorldClocksTest.java
@@ -13,6 +13,10 @@ public class HotelWorldClocksTest {
     @Test
     public void the_time_of_London_clock_should_be_1_after_the_phone_clock_is_set_to_9_Beijing_time() {
         CityClock londonClock = new CityClock("London", 0);
+        PhoneClock phoneClock = new PhoneClock(8);
+
+        phoneClock.setLocalTime(9);
+
         assertEquals(1, londonClock.getLocalTime());
     }
 }
