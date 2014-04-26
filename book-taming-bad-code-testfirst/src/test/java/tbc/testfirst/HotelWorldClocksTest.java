1bf90c195ddb9282f46a4389ce51c9ab5e4462f7
Ben Wu <wubin28@gmail.com>
2014-04-26 22:47:39 +0800

    Split the test into 3 tests for the 3 city clocks separately.

--- book-taming-bad-code-testfirst/src/test/java/tbc/testfirst/HotelWorldClocksTest.java
+++ book-taming-bad-code-testfirst/src/test/java/tbc/testfirst/HotelWorldClocksTest.java
@@ -13,15 +13,11 @@ import static org.junit.Assert.assertEquals;
  */
 public class HotelWorldClocksTest {
     @Test
-    public void the_time_of_clock_London_Moscow_NewYork_should_be_1_5_20_respectively_after_the_phone_clock_is_set_to_9_Beijing_time() {
+    public void the_time_of_clock_London_should_be_1_after_the_phone_clock_is_set_to_9_Beijing_time() {
         // Arrange
         CityClock londonClock = new CityClock("London", 0);
-        CityClock moscowClock = new CityClock("Moscow", 4);
-        CityClock newYorkClock = new CityClock("NewYork", -5);
         List hotelWorldClocks = new ArrayList<CityClock>();
         hotelWorldClocks.add(londonClock);
-        hotelWorldClocks.add(moscowClock);
-        hotelWorldClocks.add(newYorkClock);
         PhoneClock phoneClock = new PhoneClock(8);
         phoneClock.setHotelWorldClocks(hotelWorldClocks);
 
@@ -30,7 +26,37 @@ public class HotelWorldClocksTest {
 
         // Assert
         assertEquals(1, londonClock.getLocalTime());
+    }
+
+    @Test
+    public void the_time_of_clock_Moscow_should_be_5_after_the_phone_clock_is_set_to_9_Beijing_time() {
+        // Arrange
+        CityClock moscowClock = new CityClock("Moscow", 4);
+        List hotelWorldClocks = new ArrayList<CityClock>();
+        hotelWorldClocks.add(moscowClock);
+        PhoneClock phoneClock = new PhoneClock(8);
+        phoneClock.setHotelWorldClocks(hotelWorldClocks);
+
+        // Act
+        phoneClock.setLocalTime(9);
+
+        // Assert
         assertEquals(5, moscowClock.getLocalTime());
+    }
+
+    @Test
+    public void the_time_of_clock_NewYork_should_be_20_after_the_phone_clock_is_set_to_9_Beijing_time() {
+        // Arrange
+        CityClock newYorkClock = new CityClock("NewYork", -5);
+        List hotelWorldClocks = new ArrayList<CityClock>();
+        hotelWorldClocks.add(newYorkClock);
+        PhoneClock phoneClock = new PhoneClock(8);
+        phoneClock.setHotelWorldClocks(hotelWorldClocks);
+
+        // Act
+        phoneClock.setLocalTime(9);
+
+        // Assert
         assertEquals(20, newYorkClock.getLocalTime());
     }
 }
