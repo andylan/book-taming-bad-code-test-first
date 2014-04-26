e6ee06c2918af4c250937f7f5f6f7fabb761f8db
Ben Wu <wubin28@gmail.com>
2014-04-26 22:29:21 +0800

    Renamed the test to be the_time_of_clock_London_Moscow_NewYork_should_be_1_5_20_respectively_after_the_phone_clock_is_set_to_9_Beijing_time() and added the test code for the time of New York.

--- book-taming-bad-code-testfirst/src/test/java/tbc/testfirst/HotelWorldClocksTest.java
+++ book-taming-bad-code-testfirst/src/test/java/tbc/testfirst/HotelWorldClocksTest.java
@@ -13,13 +13,15 @@ import static org.junit.Assert.assertEquals;
  */
 public class HotelWorldClocksTest {
     @Test
-    public void the_time_of_clock_London_and_Moscow_should_be_1_and_5_respectively_after_the_phone_clock_is_set_to_9_Beijing_time() {
+    public void the_time_of_clock_London_Moscow_NewYork_should_be_1_5_20_respectively_after_the_phone_clock_is_set_to_9_Beijing_time() {
         // Arrange
         CityClock londonClock = new CityClock("London", 0);
         CityClock moscowClock = new CityClock("Moscow", 4);
+        CityClock newYorkClock = new CityClock("NewYork", -5);
         List hotelWorldClocks = new ArrayList<CityClock>();
         hotelWorldClocks.add(londonClock);
         hotelWorldClocks.add(moscowClock);
+        hotelWorldClocks.add(newYorkClock);
         PhoneClock phoneClock = new PhoneClock(8);
         phoneClock.setHotelWorldClocks(hotelWorldClocks);
 
@@ -29,5 +31,6 @@ public class HotelWorldClocksTest {
         // Assert
         assertEquals(1, londonClock.getLocalTime());
         assertEquals(5, moscowClock.getLocalTime());
+        assertEquals(20, newYorkClock.getLocalTime());
     }
 }
