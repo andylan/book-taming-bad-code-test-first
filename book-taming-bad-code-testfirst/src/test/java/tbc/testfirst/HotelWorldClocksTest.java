a6d00b55fc4c37839fddaca38e5e613e0138a43d
Ben Wu <wubin28@gmail.com>
2014-04-26 20:56:28 +0800

    Injected a list with two CityClock instances into the PhoneClock object in the test the_time_of_clock_London_and_Moscow_should_be_1_and_5_respectively_after_the_phone_clock_is_set_to_9_Beijing_time().

--- book-taming-bad-code-testfirst/src/test/java/tbc/testfirst/HotelWorldClocksTest.java
+++ book-taming-bad-code-testfirst/src/test/java/tbc/testfirst/HotelWorldClocksTest.java
@@ -3,6 +3,8 @@ package tbc.testfirst;
 import org.junit.Test;
 
 import java.time.zone.ZoneOffsetTransitionRule;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 
@@ -23,6 +25,16 @@ public class HotelWorldClocksTest {
 
     @Test
     public void the_time_of_clock_London_and_Moscow_should_be_1_and_5_respectively_after_the_phone_clock_is_set_to_9_Beijing_time() {
+        CityClock londonClock = new CityClock("London", 0);
+        CityClock moscowClock = new CityClock("Moscow", 4);
+        List hotelWorldClocks = new ArrayList<CityClock>();
+        hotelWorldClocks.add(londonClock);
+        hotelWorldClocks.add(moscowClock);
+        PhoneClock phoneClock = new PhoneClock(8);
+        phoneClock.setHotelWorldClocks(hotelWorldClocks);
+
+        phoneClock.setLocalTime(9);
+
         assertEquals(1, londonClock.getLocalTime());
         assertEquals(5, moscowClock.getLocalTime());
     }
