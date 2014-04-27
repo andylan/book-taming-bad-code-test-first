545f4bfa58a95c9f7b85f0fcebc18cb313878e2a
Ben Wu <wubin28@gmail.com>
2014-04-27 22:33:36 +0800

    Created a List field cityClocks in class WorldClockSystem.

--- book-taming-bad-code-testfirst/src/main/java/tbc/testfirst/WorldClockSystem.java
+++ book-taming-bad-code-testfirst/src/main/java/tbc/testfirst/WorldClockSystem.java
@@ -1,9 +1,14 @@
 package tbc.testfirst;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by benwu on 14-4-27.
  */
 public class WorldClockSystem {
+    private List<CityClock> cityClocks = new ArrayList<CityClock>();
+
     public void attach(CityClock cityClock) {
         cityClocks.add(cityClock);
     }
