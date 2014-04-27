b13399a01c5e3dfffbae6b9a4a118142454c0676
Ben Wu <wubin28@gmail.com>
2014-04-27 23:39:46 +0800

    Removed the old 'hotelWorldClocks' code from class PhoneClock."

--- book-taming-bad-code-testfirst/src/main/java/tbc/testfirst/PhoneClock.java
+++ book-taming-bad-code-testfirst/src/main/java/tbc/testfirst/PhoneClock.java
@@ -1,14 +1,11 @@
 package tbc.testfirst;
 
-import java.util.List;
-
 /**
  * Created by benwu on 14-4-25.
  */
 public class PhoneClock {
 
     private final int utcOffset;
-    private List<CityClock> hotelWorldClocks;
     private WorldClockSystem worldClockSystem;
 
     public PhoneClock(int utcOffset) {
@@ -17,15 +14,7 @@ public class PhoneClock {
 
     public void setLocalTime(int localTime) {
         // TODO: The local time of the PhoneClock object should be set. Add a test for it at first.
-        for (CityClock cityClock : this.hotelWorldClocks) {
-            cityClock.setUtcZeroTime(localTime - utcOffset);
-        }
-
         worldClockSystem.updateAllClockTime(localTime - utcOffset);
-    }
-
-    public void setHotelWorldClocks(List hotelWorldClocks) {
-        this.hotelWorldClocks = hotelWorldClocks;
     }
 
     public void setWorldClockSystem(WorldClockSystem worldClockSystem) {
